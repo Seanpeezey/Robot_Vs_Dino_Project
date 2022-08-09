6c9d643fd2155f18e2c9196f502610d001569332
Sean Perinuzzi <sperinuzzi@gmail.com>
2022-08-09 09:05:04 -0400

    Set up the files and defined the classes using the UML diagram.

---
+++ robot.py
@@ -0,0 +1,13 @@
+from weapon import Weapon
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.active_weapon = ''
+
+    def attack(self,dinosaur):
+        pass
+
+
+        
